d5f2bc1442d6df8e1a667c6a6e759e1822336691
Marcos <marcosrvlzero69@gmail.com>
2023-01-20 19:44:28 -0500

    Project Completed

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+//Middlewares
+//Using CORS allow requests from other domains
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
+//Endpoint for GET requests to the root
+//API answers with the index page
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -15,6 +19,11 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//Endpoint for GET requests to the /api route with a date param.
+//date param should be a number or string that can be converter using Date() constructor
+//to a Date Object and then API should answer a JSON like 
+//{unix: unixFormattedDateValue, utc: utcFormattedDateValue};
+//otherwise the API answers with a JSON with the key error and the value "Invalid Date"
 app.get("/api/:date", (req, res) => {
   const reqDate = req.params.date;
   let date;
@@ -30,6 +39,9 @@ app.get("/api/:date", (req, res) => {
     : res.json({ error: "Invalid Date" });
 });
 
+//Endpoint for GET request to the /api route without date param.
+//API should answer a JSON like {unix: unixFormattedDateValue, utc: utcFormattedDateValue}
+//using the actual date for the Date value
 app.get("/api/",(req,res)=>{
   const date = new Date();
   unixDate = date.getTime();
