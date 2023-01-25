e27e33a631c31efc461a8b74ff9e917a20710b9d
Marcos <marcosrvlzero69@gmail.com>
2023-01-24 22:32:00 -0500

    Comments Added

--- index.js
+++ index.js
@@ -2,28 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 3000;
 
 //Middlewares
-//Using CORS allow requests from other domains
+//Enable requests from any domain
 app.use(cors({ optionsSuccessStatus: 200 }));
+//Serving static files
 app.use(express.static('public'));
 
-//Endpoint for GET requests to the root
-//API answers with the index page
+//Routing
+//Returns index page
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
-//Endpoint for GET requests to the /api route with a date param.
-//date param should be a number or string that can be converter using Date() constructor
-//to a Date Object and then API should answer a JSON like 
-//{unix: unixFormattedDateValue, utc: utcFormattedDateValue};
-//otherwise the API answers with a JSON with the key error and the value "Invalid Date"
+//Returns a JSON with the unix and utc values for the date param
 app.get("/api/:date", (req, res) => {
   const reqDate = req.params.date;
   let date;
@@ -39,16 +34,14 @@ app.get("/api/:date", (req, res) => {
     : res.json({ error: "Invalid Date" });
 });
 
-//Endpoint for GET request to the /api route without date param.
-//API should answer a JSON like {unix: unixFormattedDateValue, utc: utcFormattedDateValue}
-//using the actual date for the Date value
+//Returns a JSON with the unix and utc values for the current date
 app.get("/api/",(req,res)=>{
   const date = new Date();
   unixDate = date.getTime();
   res.json({ unix: unixDate, utc: date.toUTCString() });
 });
 
-// listen for requests :)
-const listener = app.listen(PORT, () => {
+//Starts listening for requests
+app.listen(PORT, () => {
   console.log('Server listening on port:', PORT);
 });
